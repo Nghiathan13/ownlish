2875de6913268149ba83e035ca8fd534a1c77928
Nghiathan13 <thanminhnghia3@gmail.com>
2026-06-14 16:50:57 +0700

    Refine layout table

--- apps/web/src/features/vocabulary/components/VocabularyTable.tsx
+++ apps/web/src/features/vocabulary/components/VocabularyTable.tsx
@@ -9,9 +9,9 @@ import { EditIcon } from "@/shared/ui/icons/EditIcon";
 import { SelectCheckbox } from "@/shared/ui/SelectCheckbox";
 
 const VOCABULARY_TABLE_COLUMN_WIDTH = {
-  level: "w-[15ch]",
-  nextReview: "w-[30ch]",
-  actions: "w-[20ch]",
+  level: "w-[4rem]",
+  nextReview: "w-[8rem]",
+  actions: "w-[5rem]",
 } as const;
 
 type VocabularyTableProps = {
