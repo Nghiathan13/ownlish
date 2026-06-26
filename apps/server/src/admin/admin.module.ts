471dcd1e833bb4d800452baa840599b72eeaaaa7
Nghiathan13 <thanminhnghia3@gmail.com>
2026-06-27 00:09:27 +0700

    feature: Add UsersModule to AdminModule imports for enhanced user management capabilities.

--- apps/server/src/admin/admin.module.ts
+++ apps/server/src/admin/admin.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UsersModule } from '../users/users.module';
 import { AdminTestsController } from './admin-tests.controller';
 import { AdminToeicGroupService } from './admin-toeic-group.service';
 import { ToeicGroupRawRepository } from './lib/toeic-group-raw.repository';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [AdminTestsController],
   providers: [AdminToeicGroupService, ToeicGroupRawRepository],
 })
